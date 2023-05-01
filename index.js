6ee0d7c0b053b6ce9f1faee3deb5655ea2f073e6
Fabiola De La Torre <cubias.fabiola@gmail.com>
2023-05-01 03:19:47 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+saturdayFun("bathe my dog");
+// 'This saturday, I want to bathe my dog!'
+saturdayFun();
+// 'This Saturday, I want to roller-skate!'
+
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+//'This Monday, I will go to the office.'
+mondayWork("work from home");
+//'This Monday, I will work from home.' 
+ 
+function wrapAdjective(flair = "*") {
+    return function(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`;
+    }
+}
+wrapAdjective("||")("a dedicated programmer");
+// You are a dedicated programmer||!
